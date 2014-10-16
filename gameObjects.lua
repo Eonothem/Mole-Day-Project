3ae1484823f06eeda5a75ae4b22b3440904c9fea
Eonothem <jimmyskripchuk@gmail.com>
2014-10-15 22:20:04 -0400

    Wow its made

--- gameObjects.lua
+++ gameObjects.lua
@@ -6,7 +6,7 @@ RectangleShape = class(function(r, x, y, width, height)
 
 			end)
 
-GameObject = class(RectangleShape, function(p, x, y, width, height, speed, input)
+GameObject = class(RectangleShape, function(p, x, y, width, height, speed, input, physics)
 			   RectangleShape.init(p, x, y, width, height)
 			  
 			   p.speed = speed
@@ -14,13 +14,29 @@ GameObject = class(RectangleShape, function(p, x, y, width, height, speed, input
 			   p.velocityY = 0
 
 			   p.input = input
-			   p.physics = GameObjectPhysicsComponent()
+			   p.physics = PlayerPhysicsComponent()
 
 			   end)
 
-function GameObject:update(dt, map)
+
+Collectible = class(RectangleShape, function(c, x, y, width, height, info)
+			   RectangleShape.init(c, x, y, width, height)
+			  
+			   c.info = info
+
+			   end)
+
+function GameObject:update(dt, map, world)
 	self.input:update(self)
-	self.physics:update(self, self.speed, map)
+	self.physics:update(self, self.speed, map, world)
+end
+
+function GameObject:setDialogHandler(handler)
+	self.dialog = handler
+end
+
+function Collectible:update()
+
 end
 
 --------------
@@ -58,23 +74,94 @@ function NullInputComponent:update()
 end
 
 PhysicsComponent = class()
-GameObjectPhysicsComponent = class(PhysicsComponent)
+PlayerPhysicsComponent = class(PhysicsComponent)
 
-function GameObjectPhysicsComponent:update(object, speed, map)
+function PlayerPhysicsComponent:update(object, speed, map, world)
+
+	----------------------
+	--Physical Collision--
+	----------------------
+
 	if object.velocityY ~= 0 then
         object.y = object.y+(speed*object.velocityY)
         handleVerticalCollision(object, map)
+
     end
 
 	if object.velocityX ~= 0 then
         object.x = object.x+(speed*object.velocityX)
         handleHorizontalCollision(object, map)
     end
+
+    -----------------------------
+    --Player Object Interaction--
+    -----------------------------
+
+    for i=1, table.getn(world) do
+    	--Check if it collides, and skip it if it collides with itself
+    	if checkRectCollision(object, world[i]) and world[i] ~= object then
+    		local collidedObject = world[i]
+
+    		-- Player -> Collectible
+    		--TODO: Most likely put an observer pattern here and send it to the rendering in order to render the mole fact
+    		if instanceOf(collidedObject, Collectible) then
+    			object.dialog:addText(collidedObject.info)
+    			collidedObject.isDead = true
+    		end
+
+    	end
+    end
+
 end
+
+function instanceOf (subject, super)
+	super = tostring(super)
+	local mt = getmetatable(subject)
+ 
+	while true do
+		if mt == nil then return false end
+		if tostring(mt) == super then return true end
+ 
+		mt = getmetatable(mt)
+	end	
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 -----------------------
 --COLLISION DETECTION--
 -----------------------
+
+--IF SOMETHING BREAKS HERE YOU'RE SCREWEDh
+
+function checkRectCollision(rectA, rectB)
+	return rectA.x < rectB.x + rectB.width and
+		   rectB.x < rectA.x + rectA.width and
+		   rectA.y < rectB.y + rectB.height and
+		   rectB.y < rectA.y + rectA.height 
+end
+
+function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
+end
 
 function handleHorizontalCollision(object, map)
 	local leftTile = toGrid(object.x)
--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 require("tile")
 require("class")
 require("gameObjects")
+require("camera")
 
 SCALE_FACTOR = .5
 TILE_WIDTH = 100*SCALE_FACTOR
 
-MAP_WIDTH = 4
-MAP_HEIGHT = 4
+
 
 int_map = {{1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 },
 	       {1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1 }, 
@@ -21,9 +21,23 @@ int_map = {{1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 },
 	       {1 ,0 ,0 ,0 ,0 ,0 ,0 ,1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1 },
 	       {1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1}}
 
+MOLE_FACT = "A mole is a unit of measurment that is pretty swag."
 
+MAP_WIDTH = table.getn(int_map[1])*TILE_WIDTH
+MAP_HEIGHT = table.getn(int_map)*TILE_WIDTH
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+WORLD_OBJECTS = {}
+
+FONT_SIZE =25
 
 function love.load()
+	love.window.setMode(SCREEN_WIDTH,SCREEN_HEIGHT)
+
+	love.graphics.setNewFont("novem.ttf", FONT_SIZE)
+
 
 	------------
 	--Load Map--
@@ -40,27 +54,68 @@ function love.load()
 			map[row][col] = newTile
 		end
 	end
-	
-	player = createPlayer(150,150)
-	block = GameObject(60, 200, 128, 32, 0, NullInputComponent)
 
+	dialogHandler = TextBox()
+	dialogHandler:addText("Hello world!")
+	dialogHandler:addText("Disregard!")
+
+	
+
+
+	player = createPlayer(150,150)
+	player:setDialogHandler(dialogHandler)
+
+
+	table.insert(WORLD_OBJECTS, player)
+	table.insert(WORLD_OBJECTS, createMoleFact(400,200, MOLE_FACT))
 	
 end
 
+function createMoleFact(x, y, info)
+	return Collectible(x, y, 32, 32, info)
+end
+ 
 function createPlayer(x, y)
 	return GameObject(x, y, 32, 32, 5, PlayerInputComponent)
 end
 
-
 function love.update(dt)
-	-------------------------------
-	--Player Movement & Collision--
-	-------------------------------
 
-	
-	player:update(dt, map)
+	------------------
+	--Update Objects--
+	------------------
 
+	numObjects = table.getn(WORLD_OBJECTS)
+
+	for i = 1, numObjects do
+		local object = WORLD_OBJECTS[i]
+		object:update(dt, map, WORLD_OBJECTS)
+
+		if object.isDead then
+			table.remove(WORLD_OBJECTS, i)
+		end
+	end
 	
+end
+
+function love.keypressed(key, isrepeat)
+	if key == " " then
+		dialogHandler:nextMessage()
+	end
+end
+
+TextBox = class(function(t)
+		  t.textQueue = {}
+		  t.text = nil
+		  end)
+
+
+function TextBox:addText(text)
+	table.insert(self.textQueue, text)
+end
+
+function TextBox:nextMessage()
+	table.remove(self.textQueue, 1)
 end
 
 
@@ -69,7 +124,15 @@ function toGrid(coord)
 	return math.ceil(coord/TILE_WIDTH)
 end
 
+playerCamera = Camera(0,0,1,1,0)
+guiCamera = Camera(0,0,1,1,0)
+
 function love.draw()
+	playerCamera:set()
+	playerCamera:setPosition((player.x-SCREEN_WIDTH/2)+(player.width/2), (player.y-SCREEN_HEIGHT/2)+(player.height/2))
+	playerCamera:setScale()
+	--
+
 	love.graphics.setColor(225,225,225,255)
 
 	for row = 1, table.getn(map) do
@@ -89,12 +152,35 @@ function love.draw()
 		end
 	end
 
+	love.graphics.setColor(0,255,0,255)
 
-	love.graphics.setColor(0,0,225,255)
-	love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
-	love.graphics.setColor(255,0,0,255)
-	love.graphics.rectangle("fill", block.x, block.y, block.width, block.height)
+	for i = 1, table.getn(WORLD_OBJECTS) do
+		local object = WORLD_OBJECTS[i]
+		love.graphics.rectangle("fill", object.x, object.y, object.width, object.height)
+	end
 
-	love.graphics.setColor(0,0,0,255)
+	playerCamera:unset()
 
+	love.graphics.setColor(255,255,255,255)
+	handleTextBox()
+
+	
+
+	---
+
+	--love.graphics.setColor(0,0,0,255)
+
+end
+
+function handleTextBox()
+	guiCamera:set()
+	if table.getn(dialogHandler.textQueue) > 0 then
+		local string = dialogHandler.textQueue[1]
+
+		if table.getn(dialogHandler.textQueue) > 1 then
+			string = string.."..."
+		end
+		love.graphics.print(string, 0, SCREEN_HEIGHT-FONT_SIZE)
+	end
+	guiCamera:unset()
 end
